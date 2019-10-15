a60edba0333bdbd898de23781bc425d923c3a869
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2019-10-16 01:09:39 +0200

    Setup temporary routing for index

--- src/Controller/FoodController.php
+++ src/Controller/FoodController.php
@@ -6,8 +6,11 @@
     use Symfony\Component\Routing\Annotation\Route;
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-    class FoodController
+    class FoodController extends AbstractController
     {
+        /**
+         * @Route("/food")
+         */
         public function index() {
             return new Response('<html><body>Hello world!</body></html>');
         }
