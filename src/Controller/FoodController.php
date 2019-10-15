04d987a20584c4057f8fdc2afd23dfcf4ee1a2af
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2019-10-16 01:00:54 +0200

    Correction mistake

--- src/Controller/FoodController.php
+++ src/Controller/FoodController.php
@@ -2,7 +2,7 @@
 
     namespace App\Controller;
 
-    use Symfony\Componen\HttpFoundation\Response;
+    use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing\Annotation\Route;
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
