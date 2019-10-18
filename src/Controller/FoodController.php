43695ce0ea32bba36e1e750dbd7c9351c860db1a
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2019-10-19 00:18:07 +0200

    Add path to entity

--- src/Controller/FoodController.php
+++ src/Controller/FoodController.php
@@ -2,6 +2,8 @@
 
     namespace App\Controller;
 
+    use App\Entity\Food;
+
     use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing\Annotation\Route;
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
