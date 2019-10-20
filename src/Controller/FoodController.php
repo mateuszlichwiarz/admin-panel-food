9ff15c0136509eecc7896141d4096fa90cbf4a0f
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2019-10-20 15:58:24 +0200

    Use new bundles in FoodController

--- src/Controller/FoodController.php
+++ src/Controller/FoodController.php
@@ -4,8 +4,12 @@
 
     use App\Entity\Food;
 
+    use FOS\RestBundle\Controller\FOSRestController;
+    use FOS\RestBundle\Controller\Anoonations as Rest;
+
+    use Symfony\Component\HttpFoundation\Request;
     use Symfony\Component\HttpFoundation\Response;
-    use Symfony\Component\Routing\Annotation\Route;
+    use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
     class FoodController extends AbstractController
