360a7e31b991d24f1e3f57e03cbb27591fc9a37f
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2019-10-23 16:47:55 +0200

    Use all bundles

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -1,3 +1,11 @@
 <?php
 
     namespace App\Controller;
+
+    use FOS\RestBundle\Controller\FOSRestController;
+    use FOS\RestBundle\Controller\Annotations as Rest;
+
+    use Symfony\Component\HttpFoundation\Request;
+    use Symfony\Component\HttpFoundation\Response;
+    use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
