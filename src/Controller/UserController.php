76e3b238c7415197195b53cb82b2d8b19c7db7e6
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2019-10-23 23:25:54 +0200

    Change name routing user controller

---
+++ src/Controller/UserController.php
@@ -0,0 +1,38 @@
+<?php
+
+    namespace App\Controller;
+
+    use App\Form\Type\UserType;
+
+    use App\Entity\Users;
+
+    use FOS\RestBundle\Controller\FOSRestController;
+    use FOS\RestBundle\Controller\Annotations as Rest;
+
+    use Symfony\Component\HttpFoundation\Request;
+    use Symfony\Component\HttpFoundation\Response;
+    use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+    /**
+     * @Route("/users", name="users_")
+     */
+    class UserController extends FOSRestController
+    {
+        /**
+         *  @Rest\Get("/login")
+         * 
+         * @return Response
+         */
+        public function login() {
+            $user = new Users;
+
+            $form = $this->createForm(UserType::class, $user, [
+                'method' => 'POST'
+            ]);
+            
+            return $this->render('user/login.html.twig', array(
+                'form' => $form->createView()
+            ));
+        }
+    }
