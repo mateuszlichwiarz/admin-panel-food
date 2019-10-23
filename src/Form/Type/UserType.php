91e4decd047fc07dbd16f097680d6da1ab2d0b79
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2019-10-23 20:37:07 +0200

    Use bundles for form

---
+++ src/Form/Type/UserType.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Form\Type;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\TextType;
+use Symfony\Component\Form\Extension\Core\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
