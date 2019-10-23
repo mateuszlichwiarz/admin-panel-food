d89399da816f4c41a87fe31648eec1321d13a7fc
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2019-10-23 20:47:09 +0200

    Create simple form for login

--- src/Form/Type/UserType.php
+++ src/Form/Type/UserType.php
@@ -7,3 +7,15 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\TextType;
 use Symfony\Component\Form\Extension\Core\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $buider, array $options)
+    {
+        $builder
+            ->add('login', TextType::class, array('attr' => array('class' => 'form-control')))
+            ->add('password', TextType::class, array('attr' => array('class' => 'form-control')))
+            ->add('submit', SubmitType::class, array('attr' => array('class' => 'btn btn-primary mt-3')))
+            ;
+    }
+}
