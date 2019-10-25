a96d25e69d0ac27981c8d469338e527fcf5ed999
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2019-10-25 17:53:38 +0200

    Use authenticationUtils

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,12 +9,14 @@
     use FOS\RestBundle\Controller\FOSRestController;
     use FOS\RestBundle\Controller\Annotations as Rest;
 
+    use Symfony\Component\Security\Http\Authentications\AuthenticationUtils;
     use Symfony\Component\HttpFoundation\Request;
     use Symfony\Component\HttpFoundation\Response;
     use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
     /**
+     * Class UserController
      * @Route("/users", name="_users")
      */
     class UserController extends FOSRestController
