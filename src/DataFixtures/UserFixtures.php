680d7fbd1b401ef62839c63ab203408fcadbbb53
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2019-10-25 20:37:09 +0200

    Add UserFixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,19 @@
+<?php
+
+    namespace App\DataFixtures;
+
+    use App\Entity\User;
+    use Doctrine\Bundle\FixturesBundle\Fixture;
+    use Doctrine\Common\Persistence\ObjectManager;
+    use Doctrine\ORM\EntityManager;
+    use Doctrine\ORM\EntityManagerInterface;
+    use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+    /**
+     * Class UserFixtures
+     * @package App\DataFixtures
+     */
+    class UserFixtures extends Fixture
+    {
+
+    }
