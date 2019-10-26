ad3c15e000d7e942aa1f52b4ff2bc1cab7b5ab28
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2019-10-26 21:52:07 +0200

    Little fix

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -25,41 +25,40 @@
      */
     class UserController extends FOSRestController {
     
-    /**
-    * @Route("/login", name="login")
-    *
-    * @return Response
-    */ 
-    public function login(Request $request, AuthenticationUtils $authenticationUtils) {
-        $errors = $authenticationUtils->getLastAuthenticationError();
-        $lastUsername = $authenticationUtils->getLastUsername();
-        
-        return $this->render('User/login.html.twig', [
-            'errors' => $errors,
-            'username' => $lastUsername
-        ]);
-    }
+        /**
+        * @Route("/login", name="login")
+        *
+        * @return Response
+        */ 
+        public function login(Request $request, AuthenticationUtils $authenticationUtils) {
+            $errors = $authenticationUtils->getLastAuthenticationError();
+            $lastUsername = $authenticationUtils->getLastUsername();
+            
+            return $this->render('User/login.html.twig', [
+                'errors' => $errors,
+                'username' => $lastUsername
+            ]);
+        }
 
-    /**
-     * @Rest\Get("/food")
-     */
-    public function adminPanel() {
-        $foods = $this->getDoctrine()->getRepository(Food::class)->findAll();
+        /**
+         * @Rest\Get("/food")
+         */
+        public function adminPanel() {
+            $foods = $this->getDoctrine()->getRepository(Food::class)->findAll();
 
-        $admin = 0;
+            $admin = 0;
 
-        return $this->render('food/index.html.twig', array(
-            'foods' => $foods,
-            'admin' => $admin
-        ));
-    }
-        
-    /**
-    * @Rest\Get("/logout", name="logout")
-    * 
-    * @return Response
-    */
-    public function logout() : Response {}
-    
->>>>>>> new_feature
+            return $this->render('food/index.html.twig', array(
+                'foods' => $foods,
+                'admin' => $admin
+            ));
+        }
+            
+        /**
+        * @Rest\Get("/logout", name="logout")
+        * 
+        * @return Response
+        */
+        public function logout() : Response {}
+
     }
