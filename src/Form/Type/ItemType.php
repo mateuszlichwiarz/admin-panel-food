8459bc8893fca1dc9c0fe46547a53a5a3ed1cfa7
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2019-10-27 23:52:09 +0100

    Create form builder

--- src/Form/Type/ItemType.php
+++ src/Form/Type/ItemType.php
@@ -8,3 +8,19 @@
     use Symfony\Component\Form\Extension\Core\Type\SubmitType;
     use Symfony\Component\Form\Extension\Core\Type\TextareaType;
     use Symfony\Component\Form\FormBuilderInterface;
+
+    class ItemType extends AbstractType 
+    {
+        public function buidForm(FormBuilderInterface $builder, array $options)
+        {
+            $builder
+                ->add('name',        textType::class,     array('attr' => array('class' => 'form-control')))
+                ->add('type',        textType::class,     array('attr' => array('class' => 'form-control')))
+                ->add('description', textareaType::class, array('attr' => array('class'=>'form-control')))
+                ->add('save',        SubmitType::class,   array(
+                    'label' => 'save', 
+                    'attr' => array('class' => 'btn btn-primary mt-3')
+                ))
+            ;
+        }
+    }
